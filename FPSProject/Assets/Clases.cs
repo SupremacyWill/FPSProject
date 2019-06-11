560e5929ebeb9b6bd61410c2f1442efb98fcd446
SupremacyWill <SupremacyUlt@hotmail.com>
2019-06-11 16:43:29 -0500

    FPSProject

---
+++ FPSProject/Assets/Clases.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clases : MonoBehaviour
+{
+    
+    void Start()
+    {
+     Debug.Log("Tu Madre");
+        GameObject bolota = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        bolota.GetComponent<Renderer>().material.color = Color.black;
+        bolota.transform.(5,5,5);
+    }
+}
